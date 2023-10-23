c7c120283fde9c2f53ad610fd29627ce4fb678ed
ayunierto <32336070+ayunierto@users.noreply.github.com>
2023-10-23 17:10:12 -0500

    Update and rename install2.sh to install-arch-2.sh

---
+++ install-arch-2.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+ln -sf /usr/share/zoneinfo/America/Lima /etc/localtime
+hwclock --systohc
+vim /etc/locale.gen 
+locale-gen 
+echo "LANG=es_PE.UTF-8" > /etc/locale.conf
+echo "laptop" > /etc/hostname
+echo "127.0.0.1 localhost" >> /etc/hosts 
+echo "::1 localhost" >> /etc/hosts 
+echo "127.0.0.1 laptop.localhost localhost" >> /etc/hosts 
+cat /etc/hosts
+passwd 
+useradd -m hawk
+passwd hawk
+usermod -aG wheel hawk
+pacman -S sudo
+vim /etc/sudoers
+pacman -S grub efibootmgr os-prober
+grub-install --target=x86_64-efi --efi-directory=/boot
+os-prober 
+grub-mkconfig -o /boot/grub/grub.cfg
+pacman -S networkmanager wpa_supplicant
