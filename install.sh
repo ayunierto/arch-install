75bc80f4b62b1c0554bbdc9da689444b5db04f3b
ayunierto <32336070+ayunierto@users.noreply.github.com>
2024-06-22 22:17:10 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -54,7 +54,7 @@ sudo pacman -S --noconfirm zip unzip tar p7zip
 sudo pacman -S zsh
 sudo usermod --shell /usr/bin/zsh hawk
 agregando la terminal zsh al usuario hawk
-paru -S zsh-syntax-highlighting zsh-autosuggestions # Agregando plugins a zsh
+sudo pacman-S zsh-syntax-highlighting zsh-autosuggestions # Agregando plugins a zsh
 # Instalar locate pra saber ubicacion de los archivos en el sistema
 sudo pacman -S locate
 sudo updatedb # Actualizar base de datos de locate
