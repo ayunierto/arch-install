8add2b672c289ee51b208ae51d59cd93c7816833
ayunierto <32336070+ayunierto@users.noreply.github.com>
2024-07-24 05:49:26 -0500

    Update and rename install-arch-2.sh to install-2.sh

--- install-2.sh
+++ install-2.sh
@@ -3,10 +3,10 @@
 ln -sf /usr/share/zoneinfo/America/Lima /etc/localtime
 hwclock --systohc
 echo "es_PE.UTF-8 UTF-8" > /etc/locale.gen
-vim /etc/locale.gen 
+nvim /etc/locale.gen 
 locale-gen 
 echo "LANG=es_PE.UTF-8" > /etc/locale.conf
-echo "laptop" > /etc/hostname
+echo "hawk" > /etc/hostname
 echo "127.0.0.1 localhost" >> /etc/hosts 
 echo "::1 localhost" >> /etc/hosts 
 echo "127.0.0.1 laptop.localhost localhost" >> /etc/hosts 
@@ -17,11 +17,9 @@ useradd -m hawk
 echo "Clave para el usuario hawk"
 passwd hawk
 usermod -aG wheel hawk
-pacman -S sudo vim
-vim /etc/sudoers
-pacman -S grub efibootmgr os-prober
+nvim /etc/sudoers
+pacman -S grub efibootmgr
 grub-install --target=x86_64-efi --efi-directory=/boot
-os-prober 
 grub-mkconfig -o /boot/grub/grub.cfg
 pacman -S networkmanager wpa_supplicant
 exit
